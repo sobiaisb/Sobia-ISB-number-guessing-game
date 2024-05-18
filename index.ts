b3d930c6d8f652e573d95eaf4f9a378dc2403729
sobiaisb <163506049+sobiaisb@users.noreply.github.com>
2024-05-18 00:17:47 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,21 @@
+#!usr/bin/env node
+import inquirer from "inquirer";
+
+console.log("Welcome to Sobia Isb Number Guessing Game");
+
+const randomNumber = Math.floor(Math.random() * 9 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "Enter Your Guess Number (Number Limit from 1 to 9):",
+    }
+]);
+
+if (answer.UserGuessedNumber === randomNumber) {
+    console.log("Congratulations! You Guessed the correct number.");
+} else {
+    console.log("Sorry, you guessed the wrong number. Please try again.");
+}
+
